c1cc8757daf9b04c476f5f522536abd61b8d9c6a
SimulatedGREG <simulatedgreg@gmail.com>
2015-10-21 00:23:06 -0500

    new settings panel

--- www.js
+++ www.js
@@ -5,14 +5,19 @@ app.engine('html', require('hogan-express'));
 app.use('/bower_components', express.static('bower_components'));
 app.use('/includes', express.static('includes'));
 
-app.get('/:channel', function (req, res) {
+app.get('/:channel', function(req, res) {
   res.locals = req.params;
-  res.render('../index.html');
+  res.render('chat.html');
+});
+
+app.get('/:channel/settings', function(req, res) {
+  res.locals = req.params;
+  res.render('index.html');
 });
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Listening at http://%s:%s', host, port);
 });
